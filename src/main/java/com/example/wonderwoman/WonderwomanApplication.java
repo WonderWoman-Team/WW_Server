cd908aef4e82e6b25f635cde4dfa1eff970ad125
yeni-choi <yewonchoi@ewhain.net>
2023-07-05 16:21:34 +0900

    initial settings

---
+++ src/main/java/com/example/wonderwoman/WonderwomanApplication.java
@@ -0,0 +1,13 @@
+package com.example.wonderwoman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderwomanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonderwomanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wonderwoman/WonderwomanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wonderwoman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderwomanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
