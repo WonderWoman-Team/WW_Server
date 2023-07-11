915d1d5d66ad3f8ec803c01731879c8fa5331c98
syahn <496300@naver.com>
2023-07-12 02:00:48 +0900

    FEAT: 일반 응답 dto 구현

---
+++ src/main/java/com/example/wonderwoman/common/dto/NormalResponseDto.java
@@ -0,0 +1,22 @@
+package com.example.wonderwoman.common.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NormalResponseDto {
+    private String status;
+
+    protected NormalResponseDto(String status) {
+        this.status = status;
+    }
+
+    public static NormalResponseDto success() {
+        return new NormalResponseDto("SUCCESS");
+    }
+
+    public static NormalResponseDto fail() {
+        return new NormalResponseDto("FAIL");
+    }
+}
