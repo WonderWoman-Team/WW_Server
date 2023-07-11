741988f63d5535af35c866e24fec44661552ca49
syahn <496300@naver.com>
2023-07-12 01:59:49 +0900

    FEAT: 엔티티 구현

---
+++ src/main/java/com/example/wonderwoman/member/entity/School.java
@@ -0,0 +1,11 @@
+package com.example.wonderwoman.member.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum School {
+    이화여자대학교,
+    숙명여자대학교;
+}
