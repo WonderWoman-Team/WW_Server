55d0ba2923582cb8a23d8e44540f2c6d0ea4a978
givesilverstick <silverstick21@sookmyung.ac.kr>
2023-07-29 05:11:14 +0900

    REFACTOR: DTO 사용으로 인한 수정된 코드 추가(임시 주석 처리)

--- src/main/java/com/example/wonderwoman/delivery/controller/DeliveryController.java
+++ src/main/java/com/example/wonderwoman/delivery/controller/DeliveryController.java
@@ -1,6 +1,8 @@
 package com.example.wonderwoman.delivery.controller;
 
 import com.example.wonderwoman.common.dto.NormalResponseDto;
+import com.example.wonderwoman.delivery.DeliveryPostResponseDto;
+import com.example.wonderwoman.delivery.DeliveryResponseDto;
 import com.example.wonderwoman.delivery.entity.DeliveryPost;
 import com.example.wonderwoman.delivery.request.DeliveryRequestDto;
 import com.example.wonderwoman.delivery.service.DeliveryService;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -31,26 +34,42 @@ public class DeliveryController {
 
     // 딜리버리 게시글 조회 - 전체
     @GetMapping("/post")
+
     @ResponseBody
     public ResponseEntity<List<DeliveryPost>> getAllDeliveryPosts() {
         List<DeliveryPost> deliveryPosts = deliveryService.getAllDeliveryPosts();
         return ResponseEntity.ok(deliveryPosts);
     }
 
+//    public ResponseEntity<List<DeliveryPostResponseDto>> getAllDeliveryPosts() {
+//        List<DeliveryPostResponseDto> deliveryPosts = deliveryService.getAllDeliveryPosts();
+//        return ResponseEntity.ok(deliveryPosts);
+//    }
+
+//    public ResponseEntity<List<DeliveryResponseDto>> getAllDeliveryPosts() {
+//        List<DeliveryPost> deliveryPosts = deliveryService.getAllDeliveryPosts();
+//        List<DeliveryResponseDto> responseDtos = new ArrayList<>();
+//        for (DeliveryPost deliveryPost : deliveryPosts) {
+//            DeliveryResponseDto responseDto = DeliveryResponseDto.of(deliveryPost, false);
+//            responseDtos.add(responseDto);
+//        }
+//        return ResponseEntity.ok(responseDtos);
+//    }
+
     // 딜리버리 게시글 조회 - 유형: 요청
     @GetMapping(params = "category=request")
     @ResponseBody
     public ResponseEntity<List<DeliveryPost>> getDeliveryPostsByTypeRequest() {
-        List<DeliveryPost> callDeliveryPosts = deliveryService.getDeliveryPostsByTypeRequest();
-        return ResponseEntity.ok(callDeliveryPosts);
+        List<DeliveryPost> requestDeliveryPosts = deliveryService.getDeliveryPostsByTypeRequest();
+        return ResponseEntity.ok(requestDeliveryPosts);
     }
 
     // 딜리버리 게시글 조회 - 유형: 출동
     @GetMapping(params = "category=dispatch")
     @ResponseBody
     public ResponseEntity<List<DeliveryPost>> getDeliveryPostsByTypeDispatch() {
-        List<DeliveryPost> callDeliveryPosts = deliveryService.getDeliveryPostsByTypeDispatch();
-        return ResponseEntity.ok(callDeliveryPosts);
+        List<DeliveryPost> dispatchDeliveryPosts = deliveryService.getDeliveryPostsByTypeDispatch();
+        return ResponseEntity.ok(dispatchDeliveryPosts);
     }
 
 }
--- src/main/java/com/example/wonderwoman/delivery/service/DeliveryService.java
+++ src/main/java/com/example/wonderwoman/delivery/service/DeliveryService.java
@@ -1,6 +1,8 @@
 package com.example.wonderwoman.delivery.service;
 
+import com.example.wonderwoman.delivery.DeliveryPostResponseDto;
 import com.example.wonderwoman.delivery.entity.DeliveryPost;
+import com.example.wonderwoman.delivery.entity.ReqType;
 import com.example.wonderwoman.delivery.repository.DeliveryPostRepository;
 import com.example.wonderwoman.delivery.request.DeliveryRequestDto;
 import com.example.wonderwoman.member.entity.Member;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -26,15 +29,34 @@ public class DeliveryService {
     public List<DeliveryPost> getAllDeliveryPosts() {
         return deliveryPostRepository.findAll();
     }
+//    public List<DeliveryPostResponseDto> getAllDeliveryPosts() {
+//        List<DeliveryPost> deliveryPosts = deliveryPostRepository.findAll();
+//        // DeliveryPost를 DeliveryPostResponseDto로 변환하여 리스트로 반환
+//        return deliveryPosts.stream()
+//                .map(this::convertToResponseDto)
+//                .collect(Collectors.toList());
+//    }
 
     // 게시글 조회 - 유형:요청
     public List<DeliveryPost> getDeliveryPostsByTypeRequest() {
-        return deliveryPostRepository.findByPostReqType("request");
+        return deliveryPostRepository.findByPostReqType(ReqType.REQUEST.getTypeName());
     }
 
     // 게시글 조회 - 유형:출동
     public List<DeliveryPost> getDeliveryPostsByTypeDispatch() {
-        return deliveryPostRepository.findByPostReqType("dispatch");
+        return deliveryPostRepository.findByPostReqType(ReqType.DISPATCH.getTypeName());
+    }
+
+    // DeliveryPost를 DeliveryPostResponseDto로 변환하는 메서드
+    private DeliveryPostResponseDto convertToResponseDto(DeliveryPost deliveryPost) {
+        return new DeliveryPostResponseDto(
+                deliveryPost.getPostTitle(),
+                deliveryPost.getPostReqType().getTypeName(),
+                deliveryPost.getPostNumber(),
+                deliveryPost.getSanitarySize().getSizeName(),
+                deliveryPost.getSanitaryType().getTypeName(),
+                deliveryPost.getPostStatus().getStatusName()
+        );
     }
 
 }
