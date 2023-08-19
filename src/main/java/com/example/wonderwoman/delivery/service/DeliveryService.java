681af89b5ef33c73b5060aaf724f65f1e3845397
givesilverstick <silverstick21@sookmyung.ac.kr>
2023-08-19 12:53:23 +0900

    FEAT: 게시글 상태 조회 메서드 추가

--- src/main/java/com/example/wonderwoman/delivery/service/DeliveryService.java
+++ src/main/java/com/example/wonderwoman/delivery/service/DeliveryService.java
@@ -63,6 +63,11 @@ public class DeliveryService {
         return deliveryRepositoryImpl.getSliceOfDelivery(member, reqType, school, buildings, sanitarySizes, lastId, pageable);
     }
 
-
+    // 게시글 상태 조회
+    public PostStatus findPostStatus(Member member, String postId) {
+        DeliveryPost deliveryPost = deliveryPostRepository.findByIdAndMember(Long.valueOf(postId), member)
+                .orElseThrow(() -> new RuntimeException("해당하는 게시글을 찾을 수 없습니다."));
+        return deliveryPost.getStatus();
+    }
 
 }
