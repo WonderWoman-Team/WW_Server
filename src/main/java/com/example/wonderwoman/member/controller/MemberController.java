6156193855b851afe1ffd962c84425a6ed6d786a
syahn <496300@naver.com>
2023-08-19 15:37:17 +0900

    REFACTOR : memberController로 이동

--- src/main/java/com/example/wonderwoman/member/controller/MemberController.java
+++ src/main/java/com/example/wonderwoman/member/controller/MemberController.java
@@ -1,14 +1,13 @@
 package com.example.wonderwoman.member.controller;
 
 import com.example.wonderwoman.login.CurrentUser;
-import com.example.wonderwoman.mail.service.MemberService;
 import com.example.wonderwoman.member.entity.Member;
+import com.example.wonderwoman.member.request.ChangeMemberInfoRequest;
 import com.example.wonderwoman.member.response.MemberResponseDto;
+import com.example.wonderwoman.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +19,11 @@ public class MemberController {
     public ResponseEntity<MemberResponseDto> myInfo(@CurrentUser Member member) {
         MemberResponseDto responseDto = memberService.getMyInfo(member);
         return ResponseEntity.ok(responseDto);
+    }
+
+    @PutMapping("/myInfo")
+    public ResponseEntity<MemberResponseDto> changeMyInfo(@CurrentUser Member member, @RequestBody ChangeMemberInfoRequest request) {
+        memberService.changeMemberInfo(member, request);
+        return ResponseEntity.ok(memberService.getMyInfo(member));
     }
 }
